8718ca664b916f4f73807042d6d1c3998940dfdc
adityapratapsinghcse <adityapratapsingh728@gmail.com>
2026-06-19 16:28:52 +0530

    Complete the Alogorithms

--- STL/Algorithms/heap.cpp
+++ STL/Algorithms/heap.cpp
@@ -6,21 +6,66 @@ using namespace std;
 
 int main(){
     
-    vector<int> myArray;
-
-    myArray.push_back(40);
-    myArray.push_back(20);
-    myArray.push_back(30);
-    myArray.push_back(40);
-    myArray.push_back(10);
+    vector<int> myHeap = {20, 10, 40, 30, 50};
+    cout << "Original Vector:\n"; 
+    for(int value : myHeap) { 
+        cout << value << " "; 
+    } 
+    cout << endl;
     
-    sort(myArray.begin(),myArray.end());
-    cout << "Intial Array: " ;
-    for(auto it = myArray.begin() ; it != myArray.end(); it++){
-        cout << *it << " ";
-    }
-    cout <<endl;
+    
+    // Heapifying the Vector
+
+    make_heap(myHeap.begin(),myHeap.end());
+    cout << "After Heap:\n"; 
+    for(int value : myHeap) { 
+        cout << value << " "; 
+    } 
+    cout << endl;
+    
+    //Cheking the Top Element in Max-Heap
+
+    cout << "The Top Element is :"<< myHeap.front() << endl;
+
+    // Cheking given is Heap or Not 
+
+    bool ans = is_heap(myHeap.begin(),myHeap.end());    
+    cout << "\nIs Heap : " << ans << endl;
+    
+
+    // Pushing An Element into Heap
+
+    myHeap.push_back(60); 
+    cout << "Normal Pushback\n " ;
+    for(int value : myHeap) { 
+        cout << value << " "; 
+    } 
+    cout << endl;
+    push_heap( myHeap.begin(), myHeap.end() ); 
+    cout << "\nAfter push_heap(60):\n"; 
+    for(int value : myHeap) { 
+        cout << value << " "; 
+    } 
+    cout << endl;
+
+    //Popping An Element fromt he Heap
+    pop_heap( myHeap.begin(), myHeap.end() ); 
+    cout << "\nAfter pop_heap():\n"; 
+    for(int value : myHeap) { 
+        cout << value << " "; 
+    } 
+    cout << endl; 
+    cout << "Removed Element : " << myHeap.back() << endl; 
+    myHeap.pop_back();
+
+
+    //Sorting the Heap
+    sort_heap( myHeap.begin(), myHeap.end() ); 
+    cout << "\nAfter sort_heap():\n"; 
+    for(int value : myHeap) { 
+        cout << value << " "; 
+    } 
+    cout << endl;
 
     return 0;
-
 }
--- STL/Algorithms/set.cpp
+++ STL/Algorithms/set.cpp
@@ -2,25 +2,61 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
- 
 
-int main(){
-    
-    vector<int> myArray;
+int main()
+{
+    vector<int> first  = {1,2,3,4,5};
+    vector<int> second = {3,4,5,6,7};
 
-    myArray.push_back(40);
-    myArray.push_back(20);
-    myArray.push_back(30);
-    myArray.push_back(40);
-    myArray.push_back(10);
-    
-    sort(myArray.begin(),myArray.end());
-    cout << "Intial Array: " ;
-    for(auto it = myArray.begin() ; it != myArray.end(); it++){
-        cout << *it << " ";
+    // ==================================================
+    // SET UNION
+    // ==================================================
+
+    vector<int> result;
+
+    set_union(first.begin(),first.end(),second.begin(),second.end(),back_inserter(result));
+    cout << "Set Union : ";
+    for(int value : result){
+        cout << value << " ";
     }
-    cout <<endl;
+    cout << endl;
 
+    // ==================================================
+    // SET INTERSECTION
+    // ==================================================
+
+    result.clear();
+
+    set_intersection(first.begin(),first.end(),second.begin(),second.end(),back_inserter(result));
+    cout << "Set Intersection : ";
+    for(int value : result){
+        cout << value << " ";
+    }
+    cout << endl;
+
+    // ==================================================
+    // SET DIFFERENCE
+    // ==================================================
+
+    result.clear();
+
+    set_difference(first.begin(),first.end(),second.begin(),second.end(),back_inserter(result));
+    cout << "Set Difference (First - Second) : ";
+    for(int value : result){
+        cout << value << " ";
+    }
+    cout << endl;
+
+    // ==================================================
+    // SET SYMMETRIC DIFFERENCE
+    // ==================================================
+
+    result.clear();
+    set_symmetric_difference(first.begin(),first.end(),second.begin(),second.end(),back_inserter(result));
+    cout << "Set Symmetric Difference : ";
+    for(int value : result){
+        cout << value << " ";
+    }
+    cout << endl;
     return 0;
-
 }
